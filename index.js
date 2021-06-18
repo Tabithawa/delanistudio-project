bee53b3d94bdac30f56db629ffdd6b1e42ef9b1a
Tabithawa <tabitha.wainaina@student.moringaschool.com>
2021-06-18 17:09:01 +0300

    add pop up alert javascript function

--- index.js
+++ index.js
@@ -16,3 +16,11 @@ $(document).ready(function() {
     });
 
 });
+// ALERT TO THE USER YOU GOT THEIR MESSAGE
+function getInput() {
+    event.preventDefault();
+    var namesGiven = parseInt(document.getElementById("name").value);
+
+    alert(namesGiven + " we have received your message. Thank you for reaching out to us.");
+}
+
