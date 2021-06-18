9290328389741306b209dd60ad6fbe86b6b4ac49
Tabithawa <85102745+Tabithawa@users.noreply.github.com>
2021-06-18 17:41:02 +0300

    the name of the user can be displayed in the popup

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
 // ALERT TO THE USER YOU GOT THEIR MESSAGE
 function getInput() {
     event.preventDefault();
-    var namesGiven = parseInt(document.getElementById("name").value);
+    var namesGiven = (document.getElementById("name").value);
 
     alert(namesGiven + " we have received your message. Thank you for reaching out to us.");
 }
